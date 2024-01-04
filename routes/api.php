121567229cd86e11bc689f5fed40237d4dde2c1d
cristian <cchura.cpc@gmail.com>
2024-01-04 19:54:58 -0400

    cambios de ruta user por perfil

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/perfil', function (Request $request) {
     return $request->user();
 });
 
