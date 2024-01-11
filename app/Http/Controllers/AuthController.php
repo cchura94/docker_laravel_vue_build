fe0932addb6b6614645602819dd551e5a4fc569f
cristian <cchura.cpc@gmail.com>
2024-01-11 19:57:24 -0400

    Auteticacion registro perfil y logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -28,14 +29,36 @@ class AuthController extends Controller
     }
 
     public function funRegister(Request $request){
+        // validar
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email",
+            "password" => "required",
+            "c_password" => "required|same:password"
+        ]);
+        // registrar
+        $usuario = new User();
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = bcrypt($request->password);
+        $usuario->save();
+
+        // respuesta
+        return response()->json(["message" => "Usuario registrado"], 201);
 
     }
 
     public function funProfile(Request $request){
 
+        return response()->json($request->user());
+
     }
 
     public function funLogout(Request $request){
+
+        $request->user()->tokens()->delete();
+
+        return response()->json(["message" => "Logout: Salió"], 200);
 
     }
     
